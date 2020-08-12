4cc2b8b3883fbffd444b329d654e1faa262300de
khrom <khromov05@gmail.com>
2020-08-13 02:54:10 +0300

    neko

---
+++ neko.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const Discord = require('discord.js')
+const neko = new Discord.WebhookClient('743246927130525757', 'YWITiXeNlx97rbg6gkhgbhOrrQV9Xwvdmlrn6A6bVldB1Dfa8daoTePtbeO6f9fCdR5a');
+const client = new Discord.Client();
+
+
+
+client.on('message', msg => {
+	if (msg.author.bot || msg.channel.name != 'neko') {
+		return;
+	}
+
+	if (msg.content.toLowerCase() == 'neko') {
+		axios.get('https://nekobot.xyz/api/image?type=neko').then(response => {
+			console.log('sending neko pic...')
+			neko.send(response.data.message)
+		});
+	} else if (msg.content.toLowerCase() == 'hentai neko' || msg.content.toLowerCase() == 'hneko') {
+		axios.get('https://nekobot.xyz/api/image?type=hneko').then(response => {
+			console.log('sending hentai neko pic...')
+			neko.send(response.data.message)
+		});
+	}
+})
+
+
+client.login('NzMyNjgxNDY4MjE1ODIwMzQ5.Xw4Jvw.JskD45xytRaX7lDpSehZWaguOXA');
