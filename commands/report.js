23fa272e3c5ed1a80a0e86bf22083adbab625299
Rodney_RWR <85048343+Rodney-RWR@users.noreply.github.com>
2022-11-01 10:35:15 +1100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -5,9 +5,9 @@ this.call = function (username, args, callback) {
   if (args.length !== 0) {
     username = args[0];
   } else {
-    callback('Do ' + config.prefix +  'wdr <username>');
+    callback(`Do ${config.prefix}wdr <username>`);
     return;
   }
-  callback('Player ' + username + ' had been reported. This will be reviewed ASAP by server staff.')
+  callback(`Player ${username} had been reported. This will be reviewed ASAP by server staff.`)
 };
 
